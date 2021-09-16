fd75fc2dbcf152696b9fbd549552970afe4b6ca6
SCEREZOG <cerezosoledad353Q@gmail.com>
2021-09-16 05:53:18 -0400

    Agregar archivos de proyecto.

---
+++ ejemplo.10/principal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+	std::cout << "mi nombre es: SOLEDAD CEREZO GUZMAN" << std::endl;
+	std::cout << "mi cedula de identidad es: 7573194" << std::endl;
+	std::cout << "mi carnet universitario es:35-5130" << std::endl;
+	std::cout << "carrera universitaria: Ingenieria De Sistemas" << std::endl;
+
+	return 0;
+}
